001dc7fc97897d15d8b5893d7fcefe5b4fb85770
Akseli Kolari <akzuu@pm.me>
2023-03-22 17:49:35 +0200

    WIP: try things

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::env;
+
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
+#[get("/post")]
+fn get_post_log() -> &'static str {
     "Hello, world!"
+}
+
+#[post("/post")]
+fn notify_post() -> () {
+    for (key, value) in env::vars() {
+        println!("{key}: {value}");
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![get_post_log, notify_post])
 }
